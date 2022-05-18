6fd63ed14f1d0cc62a275cc9b41ec38ec5301176
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2022-05-18 11:51:42 -0700

    made some changes

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Dropdown.css";
 import { ItemInfo } from "../types/CustomTypes";
 
@@ -16,8 +16,23 @@ const Dropdown: React.FC<Props> = ({ setSortBy }) => {
       value: "Email",
     },
   ];
+  const dropdownContainerRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLUListElement>(null);
   const [open, setOpen] = useState<boolean>(false);
   const [selection, setSelection] = useState<ItemInfo[]>([]);
+
+  useEffect(() => {
+    if (dropdownRef.current !== null) {
+      if (open && dropdownContainerRef.current !== null) {
+        dropdownContainerRef.current.style.height = `${
+          dropdownRef.current.getBoundingClientRect().height
+        }`;
+      } else if (!open && dropdownContainerRef.current !== null) {
+        dropdownContainerRef.current.style.height = "0px";
+      }
+    }
+  }, [open]);
+
   const toggle = () => setOpen(!open);
   const handleClick = (item: ItemInfo) => {
     if (!selection.find((current) => current.id === item.id)) {
@@ -35,26 +50,31 @@ const Dropdown: React.FC<Props> = ({ setSortBy }) => {
     <div className="dropdown-wrapper">
       <div tabIndex={0} className="dropdown-header">
         <div className="dropdown-header">
-          <button onClick={() => toggle()}>Sort by </button>
+          <button className="dropdown-toggle" onClick={() => toggle()}>
+            Sort by{" "}
+          </button>
         </div>
       </div>
       {open && (
-        <ul className="list">
-          {items.map((item) => (
-            <li className="list-item" key={item.id}>
-              <button
-                type="button"
-                onClick={() => {
-                  handleClick(item);
-                  setSortBy(item.value);
-                }}
-              >
-                <span>{item.value}</span>
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div className="dropdown-container" ref={dropdownContainerRef}>
+          <ul className="list" ref={dropdownRef}>
+            {items.map((item) => (
+              <li className="list-item" key={item.id}>
+                <button
+                  type="button"
+                  onClick={() => {
+                    handleClick(item);
+                    setSortBy(item.value);
+                  }}
+                >
+                  <span className="list-title">{item.value}</span>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
+      )
     </div>
   );
 };
